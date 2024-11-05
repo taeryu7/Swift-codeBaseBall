8b6cb2b837f3ca7b7561184df1b49c1625b37f54
taeryu7 <taeryu97@gmail.com>
2024-11-05 14:00:18 +0900

    test

---
+++ BaseBall/BaseballGame.swift
@@ -0,0 +1,7 @@
+//
+//  BaseballGame.swift
+//  BaseBall
+//
+//  Created by 유태호 on 11/5/24.
+//
+
---
+++ BaseBall/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  BaseBall
+//
+//  Created by on 11/5/24.
+//
+
+// main.swift 파일
+// 프로젝트 생성시 자동 생성됨
+
+//let game = BaseballGame()
+//game.start() // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+
+
