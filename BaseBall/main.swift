f886ea263928f098fa7cc577073e921d5764c336
taeryu7 <taeryu97@gmail.com>
2024-11-05 17:09:49 +0900

    lv1 Fix

--- BaseBall/main.swift
+++ BaseBall/main.swift
@@ -4,6 +4,25 @@
 //
 //  Created by on 11/5/24.
 //
+/**
+ // main.swift 파일
+ // 프로젝트 생성시 자동 생성됨
+
+ let game = BaseballGame()
+ game.start() // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+
+
+ // BaseballGame.swift 파일 생성
+ class 혹은 struct {
+     func start() {
+         let answer = makeAnswer() // 정답을 만드는 함수
+     }
+     
+     func makeAnswer() -> Int {
+         // 함수 내부를 구현하기
+     }
+ }
+ */
 
 import Foundation
 
