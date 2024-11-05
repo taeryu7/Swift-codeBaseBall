bae4044fd33a977ec9269ea70146ca7d6be08480
taeryu7 <taeryu97@gmail.com>
2024-11-05 16:27:44 +0900

    lv.1

--- BaseBall/main.swift
+++ BaseBall/main.swift
@@ -5,10 +5,19 @@
 //  Created by on 11/5/24.
 //
 
-// main.swift 파일
-// 프로젝트 생성시 자동 생성됨
+import Foundation
 
-//let game = BaseballGame()
-//game.start() // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+var randomNumbers: Set<Int> = []    // 숫자 담을 int 배열 세팅
+var answerNumbers: [Int] = []       // 정답 저장할 변수(배열) 생성
+
+while randomNumbers.count < 3 {
+    let numbers = Int.random(in: 1...9)
+    randomNumbers.insert(numbers)
+}
+
+
+answerNumbers = Array(randomNumbers)
+print("정답은: \(randomNumbers)") // 정답
+
 
 
